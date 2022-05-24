15b524009853daeacdf37462e0791588eb9d952f
Rafaelfrb <rafael.frb@hotmail.com>
2022-05-24 10:15:19 -0300

    Improving and testing main.js with the use of for instead of while

--- main.js
+++ main.js
@@ -4,14 +4,25 @@ function tocaSom (idElementoAudio) {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++){
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; // template string
+
+    listaDeTeclas[contador].onclick = function() {
+        tocaSom(idAudio);
+    };    
+}
+
+/*let contador = 0;
 
 while (contador < listaDeTeclas.length) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-    // template string
-    const idAudio = `#som_${instrumento}`;
+    const idAudio = `#som_${instrumento}`; // template string
 
     listaDeTeclas[contador].onclick = function() {
         tocaSom(idAudio);
@@ -19,6 +30,4 @@ while (contador < listaDeTeclas.length) {
 
     contador = contador + 1;
 
-    console.log(contador);
-
-}
+}*/
